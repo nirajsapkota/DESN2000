9ca09a11c6118613168ed2d761403d0a14616473
Ryan Nguyen <z5257129@unsw.edu.au>
2020-08-06 15:57:16 +1000

    Finished onboarding pages.

--- ui/Screens/OnboardingScreen/index.tsx
+++ ui/Screens/OnboardingScreen/index.tsx
@@ -4,10 +4,11 @@
  * - [x] Plan aesthetic using Figma.
  * - [x] Create assets.
  * - [x] Prototype pages.
- * - [~] Create 1st page.
- * - [ ] Create 2nd page.
- * - [ ] Create 3rd page.
- * - [ ] Create 4th page.
+ * - [x] Create 1st page.
+ * - [x] Create 2nd page.
+ * - [x] Create 3rd page.
+ * - [x] Create 4th page.
+ * - [x] Polish all pages.
  */
 
 import React, { FC, useState } from 'react';
@@ -27,107 +28,174 @@ interface OnboardingScreenProps {
 
 const OnboardingScreen: FC<OnboardingScreenProps> = ({ navigation }) => {
     return (
-        <Swiper style={STYLES.container} index={0} loop={false}>
-            <ScrollView contentContainerStyle={S.slide}>
-                <Logo width={220} height={60} />
+        <Swiper style={S.container} index={0} loop={false}>
+            <SafeAreaView style={S.slide}>
+                <Logo style={S.logo} width={220} height={60} />
 
-                <Image source={require('./hd_opal_cards_four.png')} style={{ width: 330, height: 225 }} resizeMode='cover' />
+                <Image source={require('./hd_opal_cards_four.png')} style={S.imageOpal} resizeMode='cover' />
                 <Text style={S.title}>Welcome to the NSW Transport App</Text>
                 <Text style={S.subtitle}>Log in to your Opal Account to manage and pay with Opal cards on your account.</Text>
 
                 <View style={S.signupButtonContainer}>
                     <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                        <Neumorphic style={S.signupButton} width={60} height={60} radius={15} background={COLORS.BLUE2} centered>
+                        <Neumorphic style={S.signupButton} width={"100%"} height={50} radius={15} background={COLORS.BLUE2} centered>
                             <Text style={S.signupText}>Login</Text>
                         </Neumorphic>
                     </TouchableOpacity>
-                {/* </View> */}
 
-                <Text style={S.whisperText}>or alternatively,</Text>
+                    <Text style={S.whisperText}>or alternatively,</Text>
 
-                {/* <View style={S.signupButtonContainer}> */}
                     <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-                        <Neumorphic style={S.signupButton} width={60} height={60} radius={15} background={COLORS.BLUE2} centered>
+                        <Neumorphic style={S.signupButton} width={"100%"} height={50} radius={15} background={COLORS.BLUE2} centered>
                             <Text style={S.signupText}>Signup</Text>
                         </Neumorphic>
                     </TouchableOpacity>
                 </View>
 
-                <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
-                    <Neumorphic width={60} height={60} radius={15} background={COLORS.PRIMARY} centered >
-                        <Text>Skip</Text>
-                    </Neumorphic>
-                </TouchableOpacity>
+                <View style={S.skipContainer}>
+                    <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
+                        <Neumorphic width={60} height={40} radius={15} background={COLORS.PRIMARY} centered >
+                            <Text>Skip</Text>
+                        </Neumorphic>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
 
-            </ScrollView>
-            <View style={S.slide}>
-                <Logo width={220} height={60} />
+            <SafeAreaView style={S.slide}>
+                <Logo style={S.logo} width={220} height={60} />
+
                 <Image source={require('./payments1.png')} style={S.imagePhone} resizeMode='cover' />
                 <Text style={S.title}>Pay with your phone</Text>
                 <Text style={S.subtitle}>Tap-on and off, top-up and manage cards you’ve scanned to your phone or attached to your account.</Text>
-            </View>
-            <View style={S.slide}>
-                <Logo width={220} height={60} />
+
+                <View style={S.skipContainer}>
+                    <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
+                        <Neumorphic width={60} height={40} radius={15} background={COLORS.PRIMARY} centered >
+                            <Text>Skip</Text>
+                        </Neumorphic>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
+            <SafeAreaView style={S.slide}>
+                <Logo style={S.logo} width={220} height={60} />
+
                 <Image source={require('./trip1.png')} style={S.imagePhone} resizeMode='cover' />
                 <Text style={S.title}>Plan your trip</Text>
                 <Text style={S.subtitle}>Get a guided tour from one address to another using the NSW transport network. </Text>
-            </View>
-            <View style={S.slide}>
-                <Logo width={220} height={60} />
+                <View style={S.skipContainer}>
+                    <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
+                        <Neumorphic width={60} height={40} radius={15} background={COLORS.PRIMARY} centered >
+                            <Text>Skip</Text>
+                        </Neumorphic>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
+            <SafeAreaView style={S.slide}>
+                <Logo style={S.logo} width={220} height={60} />
+
                 <Image source={require('./accessibility1.png')} style={S.imagePhone} resizeMode='cover' />
                 <Text style={S.title}>Get accessibility info</Text>
-                <Text style={S.subtitle}>Tap on any trip’s or stop’s accessibility icon to get detailed, real-time information on accessibility services available.Tap on any trip’s or stop’s accessibility icon to get detailed, real-time information on accessibility services available.</Text>
-            </View>
-            <View style={S.slide}>
-                <Logo width={220} height={60} />
+                <Text style={S.subtitle}>Tap on any trip’s or stop’s accessibility icon to get detailed, real-time information on accessibility services available.</Text>
+
+                <View style={S.skipContainer}>
+                    <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
+                        <Neumorphic width={60} height={40} radius={15} background={COLORS.PRIMARY} centered >
+                            <Text>Skip</Text>
+                        </Neumorphic>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
+            <SafeAreaView style={S.slide}>
+                <Logo style={S.logo} width={220} height={60} />
+
                 <Image source={require('./accessibility2.png')} style={S.imagePhone} resizeMode='cover' />
                 <Text style={S.title}>Get help!</Text>
                 <Text style={S.subtitle}>Submit a request for assistance, and station staff will be there to help you with boarding, alighting and other emergencies.</Text>
-            </View>
+
+                <View style={S.skipContainer}>
+                    <TouchableOpacity style={S.skipButton} onPress={() => navigation.navigate("Home")}>
+                        <Neumorphic width={60} height={40} radius={15} background={COLORS.PRIMARY} centered >
+                            <Text>Finish</Text>
+                        </Neumorphic>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
         </Swiper>
     );
 }
 export default OnboardingScreen;
 
 const S = StyleSheet.create({
+    container: {
+        // alignContent: "center"
+    },
+    slide: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    logo: {
+        marginTop: 10,
+        marginBottom: 15,
+    },
     title: {
         marginLeft: -5,
+        marginBottom: 10,
         fontFamily: "Arial Rounded MT Bold",
         fontSize: 20,
         color: "#4070F4"
     },
     subtitle: {
+        padding: 5,
         fontFamily: "Arial",
         fontSize: 18,
         textAlign: "center",
-        color: "#4070F4"
+        color: "#4070F4",
+        marginBottom: 30
     },
     whisperText: {
+        alignSelf: "center",
         color: "#000000",
         fontSize: 10,
-        opacity: 0.5
-    },
-    slide: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        opacity: 0.5,
+        marginTop: 10,
+        marginBottom: 10,
     },
     signupText: {
         color: "#EDF0F4"
     },
     signupButtonContainer: {
-        flexDirection: "row",
+        width: "100%"
     },
     signupButton: {
         backgroundColor: "#4070F4",
         alignContent: "center",
-        flex: 1
+        alignSelf: "center",
+        width: "85%"
+    },
+    skipContainer: {
+        flex: 1,
+        alignContent: "flex-end",
+        justifyContent: "flex-end",
+        width: "100%",
     },
     skipButton: {
-        alignContent: "center"
+        alignSelf: "flex-end",
+        alignContent: "center",
+        marginBottom: 40,
+        marginRight: 20,
+    },
+    imageOpal: {
+        width: 330,
+        height: 225,
+        marginBottom: 15,
     },
     imagePhone: {
         width: 225,
-        height: 430
+        height: 430,
+        marginBottom: 15,
     }
 });
