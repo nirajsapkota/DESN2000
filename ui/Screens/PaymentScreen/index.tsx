365ff68a25e9d6521b24cce6e964db18efea4840
8veFable <29248979+8veStory@users.noreply.github.com>
2020-08-12 20:58:10 +1000

    Feat(Payments): Add auto-topup slider.

--- ui/Screens/PaymentScreen/index.tsx
+++ ui/Screens/PaymentScreen/index.tsx
@@ -2,10 +2,10 @@ import React, { FC, useState } from 'react';
 
 import {
   TouchableOpacity, SafeAreaView, StyleSheet,
-  ScrollView, View, Text
+  ScrollView, View, Text, Switch
 } from 'react-native';
 
-import { 
+import {
   PaymentMethodSelect, TopUpAmountSelect,
   PaymentSuccess, PaymentError, AddPaymentMethod
 } from './Components';
@@ -27,7 +27,8 @@ interface PaymentScreenProps {
 };
 
 const PaymentScreen: FC<PaymentScreenProps> = ({ navigation }) => {
-  
+
+  const [autoTopup, setAutoTopup] = useState(false);
   const [successModalVisibility, setSuccessModalVisibility] = useState(false);
   const [errorModalVisibility, setErrorModalVisibility] = useState(false);
   const [addPaymentMethodModalVisibility, setAddPaymentMethodModalVisibility] = useState(false);
@@ -54,49 +55,57 @@ const PaymentScreen: FC<PaymentScreenProps> = ({ navigation }) => {
         setVisibility={setSuccessModalVisibility}
         amountToppedUp={topUpAmount}
         cardNickname={currCardNickname}
-        />
+      />
 
       <PaymentError
         visibility={errorModalVisibility}
         setVisibility={setErrorModalVisibility}
-        />
+      />
 
       <AddPaymentMethod
         visibility={addPaymentMethodModalVisibility}
         setVisibility={setAddPaymentMethodModalVisibility}
-        />
+      />
 
       <Header
         navigation={navigation} />
 
       <ScrollView contentContainerStyle={STYLES.container}>
-      
+
         <Text style={[STYLES.title]}> Top up </Text>
 
-        <Text style={[STYLES.subtitle, {marginTop: 15}]}>
+        <Text style={[STYLES.subtitle, { marginTop: 15 }]}>
           Select card to top up
         </Text>
         <OpalCardSelector
           getActiveCardBalance={setCurrCardBalance}
           getActiveCardNickname={setCurrCardNickname} />
-      
-        <Text style={[STYLES.subtitle, {marginTop: 15}]}>
+
+        <View style={S.switchRow}>
+          <Text style={STYLES.subtitle}>Auto-topup</Text>
+          <Switch
+            value={autoTopup}
+            onValueChange={() => setAutoTopup(!autoTopup)}
+          />
+        </View>
+
+        <Text style={[STYLES.subtitle, { marginTop: 15 }]}>
           Top up amount
         </Text>
-        <TopUpAmountSelect 
+        <TopUpAmountSelect
           topUpAmount={topUpAmount}
-          setTopUpAmount={setTopUpAmount}/>
+          setTopUpAmount={setTopUpAmount} />
 
-        <View style={[STYLES.row, STYLES.spaceBetween, {width: 360}]}>
-          <Text style={[STYLES.subtitle, {color: 'black'}]}>
+        <View style={[STYLES.row, STYLES.spaceBetween, { width: 360 }]}>
+          <Text style={[STYLES.subtitle, { color: 'black' }]}>
             Balance after top up
           </Text>
-          <Text style={[STYLES.subtitle, {color: 'black'}]}>
+          <Text style={[STYLES.subtitle, { color: 'black' }]}>
             ${currCardBalance + topUpAmount}
           </Text>
         </View>
 
-        <View style={[STYLES.row, STYLES.spaceBetween, {width: 360, alignItems: 'center', marginTop: 15}]}>
+        <View style={[STYLES.row, STYLES.spaceBetween, { width: 360, alignItems: 'center', marginTop: 15 }]}>
           <Text style={[STYLES.subtitle]}>
             Payment method
           </Text>
@@ -115,8 +124,8 @@ const PaymentScreen: FC<PaymentScreenProps> = ({ navigation }) => {
         </View>
         <PaymentMethodSelect />
 
-        <View style={{marginBottom: 100, alignItems: 'center'}}>
-          <TouchableOpacity onPress={() => HandleShowReceipt()}>    
+        <View style={{ marginBottom: 100, alignItems: 'center' }}>
+          <TouchableOpacity onPress={() => HandleShowReceipt()}>
             <Neumorphic
               width={280}
               height={60}
@@ -124,10 +133,10 @@ const PaymentScreen: FC<PaymentScreenProps> = ({ navigation }) => {
               background={COLORS.ACCENT}
               centered>
 
-              <Text style={[STYLES.subtitle, {color: 'white'}]}>
+              <Text style={[STYLES.subtitle, { color: 'white' }]}>
                 Pay with Card **** 0172
               </Text>
-            
+
             </Neumorphic>
           </TouchableOpacity>
         </View>
@@ -141,4 +150,10 @@ const PaymentScreen: FC<PaymentScreenProps> = ({ navigation }) => {
 export default PaymentScreen;
 
 const S = StyleSheet.create({
+  switchRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 18,
+  }
 });
